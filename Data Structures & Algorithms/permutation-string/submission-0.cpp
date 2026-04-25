d1b0c59dc42286f24ec62992821b2aae463840fb
sandeep-8093 <nayaksandeep676@gmail.com>
2026-04-25 11:06:26 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length()> s2.length()) return false;
+        vector<int> freq1(26,0), freq2(26,0);
+        for(int i=0;i<s1.length();i++){
+            freq1[s1[i]- 'a']++;
+        }
+        for(int i=0;i<s1.length();i++){
+            freq2[s2[i] -'a']++;
+        }
+        if(freq1==freq2) return true;
+        for (int i = s1.length(); i < s2.length(); i++) {
+            freq2[s2[i] - 'a']++;                // add new char
+            freq2[s2[i - s1.length()] - 'a']--;    // remove old char
+
+            if (freq1 == freq2) return true;
+        }
+        return false;
+    }
+};
